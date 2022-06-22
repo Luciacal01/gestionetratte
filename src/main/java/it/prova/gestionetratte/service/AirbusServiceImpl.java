13e67ae955a2acfa7656083376b01c898b65de9d
Luciacal01 <luciacalabria80@gmail.com>
2022-06-22 20:08:01 +0200

    Creazione Repository, Service ed Excaption

---
+++ src/main/java/it/prova/gestionetratte/service/AirbusServiceImpl.java
@@ -0,0 +1,60 @@
+package it.prova.gestionetratte.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import it.prova.gestionetratte.model.Airbus;
+
+@Service
+public class AirbusServiceImpl implements AirbusService {
+
+	@Override
+	public List<Airbus> listAllElements() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Airbus> listAllElementsEager() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Airbus caricaSingoloElemento(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Airbus caricaSingoloElementoConFilms(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Airbus aggiorna(Airbus airbusInstance) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Airbus inserisciNuovo(Airbus airbusInstance) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void rimuovi(Airbus airbusInstance) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Airbus> findByExample(Airbus example) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
